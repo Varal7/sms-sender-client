dbcbc37888a35a76976d7ca9756c5f80b93b2d61
Victor Quach <varaal@gmail.com>
2017-02-27 01:32:02 +0100

    Add POST form

--- js/app.js
+++ js/app.js
@@ -1,7 +1,41 @@
 var home = Vue.component('home', {
   template: '#home',
+  data: function() {
+    return {
+      number: "+33600000000",
+      message: "Hello world",
+      sendingButton: "Send",
+      submitDisabled: false
+    }
+  },
+  methods: {
+    onSubmit: function() {
+      this.submitDisabled = true;
+      this.sendingButton = "Sending…";
+      var message = this.message;
+      var number = this.number;
+      var formData = new FormData();
+      formData.append('number', number);
+      formData.append('message', message);
+      this.$http.post("http://10.8.0.8/sms.php", formData)
+      .then(response => {
+        console.log(response.body[0].result)
+        this.submitDisabled = false;
+        this.sendingButton = "Send";
+      }, respone => {
+        console.log(response)
+        this.submitDisabled = false;
+        this.sendingButton = "Send";
+      });
+    }
+  }
 })
 
 new Vue({
-  el: '#app'
+  el: '#app',
+
+  http: {
+    emulateJSON: true,
+    emulateHTTP: true
+  }
 });
